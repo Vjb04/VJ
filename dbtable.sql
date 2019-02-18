6c6fab1849d21b02ed59278f84ede692742c6ed7
Vjb04 <42862210+Vjb04@users.noreply.github.com>
2019-02-18 12:03:19 +0530

    Create dbtable.sql

---
+++ dbtable.sql
@@ -0,0 +1,8 @@
+#db&table
+create database vjb;
+use vjb;
+create table vj(num int(5),name varchar(30),
+phno int(20),degree varchar(20),
+dep varchar(10),address varchar(60),
+age int(3),sex varchar(1),
+father varchar(20),mother varchar(20),siblings int(3));
